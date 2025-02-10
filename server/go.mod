cb4bd9dca6e62055c30850b3fc55374a47cb9290
ChefOmarDee <chefomardee@gmail.com>
2025-02-09 21:01:19 -0500

    removed stuff

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module BeyApply
 
 go 1.22.3
+
+require github.com/go-latex/latex v0.0.0-20240709081214-31cef3c7570e // indirect
--- server/latexUtils/latexUtils.go
+++ server/latexUtils/latexUtils.go
@@ -1,22 +1,33 @@
 package latexUtils
 
-import "fmt"
-
 type ResumeInfo struct {
-	resumeName       string
-	name             string
-	links            []string
-	technicalSkills  []string
-	workExperience   []string
-	projects         []string
-	extracurriculars []string
+	Name            string              `json:"name"`
+	Links           []string            `json:"links"`
+	Education       map[string]Degree   `json:"Education"`
+	TechnicalSkills TechnicalSkills     `json:"Technical Skills"`
+	Experience      map[string]Job      `json:"Experience"`
+	Projects        map[string][]string `json:"Projects"`
+	Clubs           map[string][]string `json:"Clubs/Organizations"`
 }
 
-func PrintHi() {
-	fmt.Println("hi")
+type Degree struct {
+	Institution string `json:"Institution"`
+	GPA         string `json:"GPA"`
+	Date        string `json:"Date"`
 }
 
-func ConvertJSONToLatex(resumeinfo ResumeInfo) {
+type TechnicalSkills struct {
+	LanguagesFrameworks []string `json:"Languages/Frameworks"`
+	Technologies        []string `json:"Technologies"`
+}
+
+type Job struct {
+	Date             string   `json:"Date"`
+	Role             string   `json:"Role"`
+	Responsibilities []string `json:"Responsibilities"`
+}
+
+func ConvertStructToLatex(resumeinfo ResumeInfo) {
 
 }
 
